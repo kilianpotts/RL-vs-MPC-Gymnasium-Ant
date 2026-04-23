b3f26cec6cf6ec359e2fe1d4138f7e7494c962b6
merlereh <merle.reh01@gmail.com>
2026-04-23 08:23:57 +0000

    Adapt devcontainer for macOS headless setup

--- gymnasium_ws/ant_test.py
+++ gymnasium_ws/ant_test.py
@@ -6,6 +6,18 @@ env = gym.make("Ant-v5", render_mode="rgb_array")
 
 obs, info = env.reset()
 
+# nur für mich
+# ersten Frame holen, damit wir Größe kennen
+frame = env.render()
+height, width, _ = frame.shape
+
+writer = cv2.VideoWriter(
+    "ant_video.mp4",
+    cv2.VideoWriter_fourcc(*"mp4v"),
+    20,  # FPS
+    (width, height)
+)
+
 for _ in range(1000):
     action = env.action_space.sample()
 
@@ -18,14 +30,21 @@ for _ in range(1000):
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
     # Show frame
-    cv2.imshow("MuJoCo Ant", frame)
+    #cv2.imshow("MuJoCo Ant", frame)
 
-    # Needed for window to update
-    if cv2.waitKey(1) & 0xFF == 27:  # press ESC to quit
-        break
+    #nur für mich 
+    writer.write(frame)
 
     if terminated or truncated:
         obs, info = env.reset()
 
+    # Needed for window to update
+    #if cv2.waitKey(1) & 0xFF == 27:  # press ESC to quit
+    #    break
+
+    #if terminated or truncated:
+   #     obs, info = env.reset()
+
+writer.release()
 env.close()
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
